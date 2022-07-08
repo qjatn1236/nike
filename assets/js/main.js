b744a411fdc40177391fd5e31d0e44f29fb72b01
qjatn1236@naver.com <qjatn1236@naver.com>
2022-07-08 12:41:58 +0900

    Nike Upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,22 +31,43 @@ $(function(){
   });
   // nav 슬라이드
 
+  const motionElement = document.querySelectorAll(`[data-motion]`)
+  motionElement.forEach(element => {
+
+    gsap.fromTo(element,{
+      yPercent:100,
+      opacity:0,
+    },{
+      scrollTrigger:{
+        trigger:element,
+        start:'top 100%',
+        end:'bottom top',
+        // [onEnter, onLeave, onEnterBack[멀리], onLeaveBack[가까이 ] ]
+        toggleActions:"play pause resume reset"
+      },
+      yPercent:0,
+      opacity:1,
+    })
+
+  })
+
   gsap.to('.sc-main .bg-area img',{
     scrollTrigger:{
       trigger:'.sc-main .img-box',
       start:'top 50%',
       end:'bottom top',
-      markers:true,
+      // markers:true,
       scrub:1,
     },
     yPercent:-20
   });
+  // 메인 이미지 컨트롤
 
   wideTl = gsap.timeline({
     scrollTrigger:{
       trigger:'.wide-area',
       start:'top top',
-      end:'200%',
+      end:'300%',
       scrub:1,
       pin:true,
     },
@@ -61,12 +82,12 @@ $(function(){
   },'m1+=0.2')
   .to('.wide-area .wide-wrap .logo-img',{
     top:'50%'
-  });
+  },'+=0.2');
   
   sampleTl = gsap.timeline({
     scrollTrigger:{
       trigger:'.sc-product',
-      start:'top 100%',
+      start:'top 30%',
       end:'bottom top',
       scrub:1,
     },
@@ -79,26 +100,48 @@ $(function(){
   .to('.sc-product .txt02',{
     xPercent:100
   },'m1')
-  
+
+  gsap.fromTo('.video-wrap',{
+    'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'
+  },{
+    scrollTrigger:{
+      trigger:".video-wrap",
+      start:"top 50%",
+      end:"bottom top",
+      // markers:true,
+      scrub:1,
+    },
+    'clip-path': 'polygon(10% 0, 90% 0%, 90% 100%, 10% 100%)'
+  })
+
+  gsap.to('.sc-slide',{
+    scrollTrigger:{
+      trigger:'.sc-slide',
+      start:'top top',
+      end:'+=300%',
+      scrub:1,
+      pin:true,
+    },
+    xPercent:-300
+  })
 
 // introduction
 
 const intro = gsap.timeline({
   scrollTrigger:{
-    trigger:'.introduction-area',
+    trigger:'.sc-introduction',
     pin:true,
     scrub:0.5,
     start:'top top',
-    end:'+=4000',
+    end:'+=3000',
     // markers:true
-  }
+  },
 })
 
 .addLabel('motion1')
-intro.from('.sc-introduction .introduction-wrap .img-wrap01',{scale:2, xPercent:-300},'motion1')
-.from('.sc-introduction .introduction-wrap .img-wrap02',{scale:2, xPercent: 300,},'motion1')
-.from('.sc-introduction .introduction-wrap .img-wrap03',{scale:2, yPercent: 100,},'motion1')
-.from('.sc-introduction .introduction-wrap p',{yPercent: 100},'motion1')
-
+intro.from('.sc-introduction .img-box01',{scale:2, xPercent:-200},'motion1')
+.from('.sc-introduction .img-box02',{scale:2, xPercent: 200,},'motion1')
+.from('.sc-introduction .img-box03',{scale:2, yPercent: 150,},'motion1')
+.from('.sc-introduction p',{yPercent: 150},'motion1')
 
 });
